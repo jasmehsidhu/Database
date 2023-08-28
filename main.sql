14c2ed2b2ee781115d16e87988c75674fb3f8d09
Jasmeh Singh <142116700+jasmehsidhu@users.noreply.github.com>
2023-08-28 10:25:42 -0600

    Create main.sql

---
+++ main.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Login-Form{
+Username STRING,
+Email STRING,
+Password STRING,
+}
+INSERT INTO Login-Form(Username,Email,Password)
+Values("jasmehsidhuu","jasmeh82@gmail.com","example")
